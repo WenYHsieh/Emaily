9a40f71c1bc0944b953dbfcabf42eeb421fb2686
WenYHsieh <menlen6205@gmail.com>
2023-04-10 23:42:50 +0800

    add mongodb config, connect oauth flow into db

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 import express from 'express'
+import './models/User.js'
 import './services/passport.js'
 import authRoutes from './routes/authRoutes.js'
+import mongoose from 'mongoose'
+import * as dotenv from 'dotenv'
 
 const app = express()
+const { parsed: keys } = dotenv.config()
+
 authRoutes(app)
+
+mongoose.connect(keys.mongoURI)
 
 const PORT = process.env.PORT || '8888'
 app.listen(PORT)
--- services/passport.js
+++ services/passport.js
@@ -1,8 +1,10 @@
+import mongoose from 'mongoose'
 import passport from 'passport'
 import { Strategy as GoogleStrategy } from 'passport-google-oauth20'
 import * as dotenv from 'dotenv'
 
 const { parsed: keys } = dotenv.config()
+const User = mongoose.model('users')
 
 passport.use(
   new GoogleStrategy(
@@ -12,6 +14,7 @@ passport.use(
       callbackURL: '/auth/google/callback',
     },
     (accessToken, refreshToken, profile, done) => {
+      new User({ googleId: profile.id }).save()
       console.log({ accessToken, refreshToken, profile })
     }
   )
