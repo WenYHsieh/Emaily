d3f8eca764128f9a87fd4e6e954e996949c9dc8a
WenYHsieh <menlen6205@gmail.com>
2023-04-16 18:00:17 +0800

    add logout api

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -12,6 +12,11 @@ const authRoutes = (app) => {
   // redirect (callback) URL
   app.get('/auth/google/callback', passport.authenticate('google'))
 
+  app.get('/api/logout', (req, res) => {
+    req.logout()
+    res.send(req.user)
+  })
+
   app.get('/api/currentUser', (req, res) => {
     res.send(req.user)
   })
