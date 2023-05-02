cd04ed11b2b59bd5876ce791a1ba61e96af658d6
WenYHsieh <menlen6205@gmail.com>
2023-05-02 22:56:15 +0800

    add handle charge

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -1,6 +1,21 @@
+const keys = require('../config/keys')
+const stripe = require('stripe')(keys.stripeSecreteKey)
+
 module.exports = (app) => {
   // handle stripe token
-  app.post('/api/stripe', (req, res) => {
-    console.log(req)
+  app.post('/api/stripe', async (req, res) => {
+    // create stripe object
+    const charge = await stripe.charges.create({
+      amount: 500,
+      currency: 'usd',
+      description: '$5 for 5 credits',
+      // 要收費的來源
+      source: req.body.id,
+    })
+
+    req.user.credits += 5
+    const user = req.user.save()
+
+    res.send(user)
   })
 }
