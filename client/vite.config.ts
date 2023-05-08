66136a580555ccaa294eb2a92202e3eb34f981f4
WenYHsieh <menlen6205@gmail.com>
2023-05-08 22:30:40 +0800

    fix client deploy env error

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import dotenv from 'dotenv'
 
+dotenv.config()
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
@@ -16,5 +18,10 @@ export default defineConfig({
       },
     },
   },
+  define: Object.keys(process.env).reduce((env, key) => {
+    env[`import.meta.env.${key}`] = JSON.stringify(process.env[key])
+    return env
+  }, {}),
+
   plugins: [react()],
 })
