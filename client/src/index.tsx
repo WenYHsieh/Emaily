69143181ed0a60bf5023dac4ccbb52412e06f863
WenYHsieh <menlen6205@gmail.com>
2023-05-23 21:47:18 +0800

    add search survey under user api

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './components/App'
 import { Provider } from 'react-redux'
 import { configureStore } from '@reduxjs/toolkit'
 import appReducer from './reducers'
 import reduxThunk from 'redux-thunk'
-
+import axios from 'axios'
+window.axios = axios
 export const store = configureStore({
   reducer: appReducer,
   middleware: (getDefaultMiddleware) =>
